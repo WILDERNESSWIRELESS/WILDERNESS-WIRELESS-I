453b288f78a5163812a32d04ec10c0a86fc57344
Brett Ian Balogh <brettbalogh@gmail.com>
2016-10-30 10:11:12 -0500

    html

--- wilderness/wilderness-wireless/adc.lua
+++
@@ -1,8 +0,0 @@
--- in you init.lua:
-if adc.force_init_mode(adc.INIT_VDD33)
-then
-  node.restart()
-  return -- don't bother continuing, the restart is scheduled
-end
-
-print("System voltage (mV):", adc.readvdd33(0))
